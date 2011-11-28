fe2fc74202380d5c786ec4a2b1e3e7a3d2fb044c
Kacy Fortner <kacy@kacyfortner.com>
2011-11-28 12:59:53 -0500

    updating login spec

--- Ruby/test/login_api_spec.rb
+++ Ruby/test/login_api_spec.rb
@@ -8,7 +8,7 @@ describe "Login_API" do
   it "should list all logins" do
     result = @@login.list()
     result.length.should > 0
-    # result["Items"].last["Id"].to_s.should == $site_id
+    #result["Items"].last["Id"].to_s.should == $site_id
   end
 
   it "should create a new login" do
@@ -29,24 +29,33 @@ describe "Login_API" do
   
   it "should list a specific login" do
     response = @@login.get($login_id)
-    response.body.should == '{"Id":' + $login_id.to_s + ',"Email":"'+ $email + '","Password":"' + $password + '","Name":"' + $fullname + '"}'
+    response.body.should == '{"Id":' + $login_id.to_s + ',"Email":"'+ $email + '","Password":"","Name":"' + $fullname + '"}'
   end
   
   it "should update a login" do
-    $email = "test+" + rand(10000).to_s + "@adzerk.com"
-    $password = "somepassword"
-    $fullname = "test person"
     new_login = {
       'Id' => $login_id,
-      'Email' => $email,
-      'Password' => $password,
       'Name' => $fullname
     }
     response = @@login.update(new_login)
     $login_id = JSON.parse(response.body)["Id"].to_s
     JSON.parse(response.body)["Email"].should == $email
-    JSON.parse(response.body)["Password"].should == $password
+    JSON.parse(response.body)["Password"].should == ""
     JSON.parse(response.body)["Name"].should == $fullname
   end
-  
+
+  it "should not be able update a login's password or email" do
+    new_login = {
+      'Id' => $login_id,
+      'Name' => $fullname,
+      'Password' => "password",
+      'Email' => "test@test.com"
+    }
+    response = @@login.update(new_login)
+    $login_id = JSON.parse(response.body)["Id"].to_s
+    JSON.parse(response.body)["Email"].should == $email
+    JSON.parse(response.body)["Password"].should == ""
+    JSON.parse(response.body)["Name"].should == $fullname
+  end
+
 end
