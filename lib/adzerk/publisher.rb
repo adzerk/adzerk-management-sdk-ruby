9dde11259503117c175351e095fc000269ffdabd
Rafael Chacon <rafaelchacon@gmail.com>
2012-10-08 14:43:40 -0700

    Refactor publishers  code.

--- lib/adzerk/publisher.rb
+++ lib/adzerk/publisher.rb
@@ -1,46 +1,16 @@
 module Adzerk
-  class Publisher
-
-    def create(data={})
-      uri = URI.parse($host + 'publisher')
-      data = { 'publisher' => data.to_json }
-      Adzerk.post_request(uri, data)
-    end
-
-    def get(id)
-      uri = URI.parse($host + 'publisher/' + id)
-      Adzerk.get_request(uri)
-    end
-
-    def list()
-      uri = URI.parse($host + 'publisher')
-      response = Adzerk.get_request(uri)
-      JSON.parse(response.body)
-    end
-
-    def update(data={})
-      uri = URI.parse($host + 'publisher/' + data["Id"].to_s)
-      data = { 'publisher' => data.to_json }
-      Adzerk.put_request(uri, data)
-    end
-
-    def delete(id)
-      uri = URI.parse($host + 'publisher/' + id + '/delete')
-      Adzerk.get_request(uri)
-    end
+  class Publisher < ApiEndpoint
 
     def earnings(data={})
-      uri = URI.parse($host + '/earnings')
-      data = { 'earnings' => data.to_json }
-      response = Adzerk.post_request(uri, data)
-      JSON.parse(response.body)
+      data = { 'earnings' => camelize_data(data).to_json }
+      response = @client.post_request('/earnings', data)
+      parse_response(response)
     end
 
     def payments(data={})
-      uri = URI.parse($host + '/payments')
-      data = { 'payments' => data.to_json }
-      response = Adzerk.post_request(uri, data)
-      JSON.parse(response.body)
+      data = { 'payments' => camelize_data(data).to_json }
+      response = @client.post_request('/payments', data)
+      parse_response(response)
     end
 
   end
--- test/publisher_api_spec.rb
+++ test/publisher_api_spec.rb
@@ -2,146 +2,80 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Publisher API" do
   
-  @@publisher = $adzerk::Publisher.new
+  before(:all) do
+    client = Adzerk::Client.new(API_KEY)
+    @publishers = client.publishers
+    @rand = rand(1000000).to_s
+  end
   
   it "should create a new publisher" do
-    $rand = rand(1000000).to_s
-    $publisher_first = 'Test ' + $rand
-    $publisher_last = 'Person ' + $rand
-    $publisher_company = 'Company ' + $rand
-    $publisher_line1 = $rand + 'st'
-    $publisher_line2 = 'Apt. ' + $rand
-    $publisher_city = $rand + 'ville'
-    $publisher_state = 'NC'
-    $publisher_zip = $rand.to_s
-    $publisher_country = 'USA'
-    $publisher_payment_option = "1"
-    $publisher_paypal_email = 'Test' + $rand + '@test.com'
-    $new_publisher = {
-     'FirstName' => $publisher_first,
-     'LastName' => $publisher_last,
-     'CompanyName' => $publisher_company,
-     'Address' => {
-       'Line1' => $publisher_line1,
-       'Line2' => $publisher_line2,
-       'City' => $publisher_city,
-       'StateProvince' => $publisher_state,
-       'PostalCode' => $publisher_zip,
-       'Country' => $publisher_country
+    new_publisher = {
+     :first_Name => 'John' + @rand,
+     :last_name => 'Doe' + @rand,
+     :company_name => 'Company' + @rand,
+     :address => {
+       :line1 => "1001 Invalid St",
+       :line2 => "Unit 302",
+       :city => "San Francisco",
+       :state_province => "CA",
+       :postal_code => "90210",
+       :country => "USA"
      },
-     'PaymentOption' => $publisher_payment_option,
-     'PaypalEmail' => $publisher_paypal_email
+     :payment_option => 1, # this is paypal
+     :paypal_email => "johndoe+" + @rand + "@johndoe.com"
     }
-    response = @@publisher.create($new_publisher)
-    $publisher_id = JSON.parse(response.body)["Id"].to_s
-    $publisher_first.should == JSON.parse(response.body)["FirstName"]
-    $publisher_last.should == JSON.parse(response.body)["LastName"]
-    $publisher_company.should == JSON.parse(response.body)["CompanyName"]
-    JSON.parse(response.body)["PaymentOption"].should == "1"
-    $publisher_paypal_email.should == JSON.parse(response.body)["PaypalEmail"]
-    
-    $address_id = JSON.parse(response.body)["Address"]["Id"].to_s
-    $publisher_line1.should == JSON.parse(response.body)["Address"]["Line1"]
-    $publisher_line2.should == JSON.parse(response.body)["Address"]["Line2"]
-    $publisher_city.should == JSON.parse(response.body)["Address"]["City"]
-    $publisher_state.should == JSON.parse(response.body)["Address"]["StateProvince"]
-    $publisher_zip.should == JSON.parse(response.body)["Address"]["PostalCode"]
-    $publisher_country.should == JSON.parse(response.body)["Address"]["Country"]
+    publisher = @publishers.create(new_publisher)
+    $publisher_id = publisher[:id]
+    publisher[:first_name].should eq('John' + @rand)
+    publisher[:last_name].should eq('Doe' + @rand)
+    publisher[:company_name].should eq('Company' + @rand)
+    publisher[:payment_option].should eq("1")
+    publisher[:paypal_email].should eq("johndoe+" + @rand + "@johndoe.com")
+    publisher[:address].should_not be_nil
+    $address_id = publisher[:address][:id].to_s
   end
   
   it "should list a specific publisher" do
-     response = @@publisher.get($publisher_id)
-     response.body.should == '{"Id":' + $publisher_id + ',"FirstName":"' + $publisher_first + '","LastName":"' + $publisher_last + '","CompanyName":"' + $publisher_company + '","Address":{"Id":' + $address_id + ',"Line1":"' + $publisher_line1 + '","Line2":"' + $publisher_line2 + '","City":"' + $publisher_city + '","StateProvince":"' + $publisher_state + '","PostalCode":"' + $publisher_zip + '","Country":"' + $publisher_country + '"},"PaypalEmail":"' + $publisher_paypal_email + '","PaymentOption":"PayPal","IsDeleted":false}'
+    publisher = @publishers.get($publisher_id)
+    publisher[:first_name].should eq('John' + @rand)
+    publisher[:last_name].should eq('Doe' + @rand)
+    publisher[:company_name].should eq('Company' + @rand)
+    publisher[:payment_option].should eq("PayPal")
+    publisher[:paypal_email].should eq("johndoe+" + @rand + "@johndoe.com")
+    publisher[:address].should_not be_nil
    end
 
   it "should update a publisher" do
-    $new_publisher = {
-      'Id' => $publisher_id,
-      'FirstName' => $publisher_first + "test",
-      'LastName' => $publisher_last + "test",
-      'CompanyName' => $publisher_company + "test",
-      'Address' => {
-       'Line1' => $publisher_line1 + "test",
-       'Line2' => $publisher_line2 + "test",
-       'City' => $publisher_city + "test",
-       'StateProvince' => $publisher_state + "test",
-       'PostalCode' => $publisher_zip + "test",
-       'Country' => $publisher_country + "test"
-      },
-      'PaymentOption' => $publisher_payment_option,
-      'PaypalEmail' => $publisher_paypal_email + "test"
+    updated_publisher = {
+     :id => $publisher_id,
+     :first_Name => 'Rafael' + @rand,
+     :last_name => 'Doe' + @rand,
+     :company_name => 'Company' + @rand,
+     :address => {
+       :line1 => "1001 Invalid St",
+       :line2 => "Unit 302",
+       :city => "San Francisco",
+       :state_province => "CA",
+       :postal_code => "90210",
+       :country => "USA"
+     },
+     :payment_option => 1, # this is paypal
+     :paypal_email => "johndoe+" + @rand + "@johndoe.com"
     }
-    response = @@publisher.update($new_publisher)
-    $publisher_id = JSON.parse(response.body)["Id"].to_s
-    ($publisher_first + "test").should == JSON.parse(response.body)["FirstName"]
-    ($publisher_last + "test").should == JSON.parse(response.body)["LastName"]
-    ($publisher_company + "test").should == JSON.parse(response.body)["CompanyName"]
-    JSON.parse(response.body)["PaymentOption"].should == "1"
-    ($publisher_paypal_email + "test").should == JSON.parse(response.body)["PaypalEmail"]
-    
-    $address_id = JSON.parse(response.body)["Address"]["Id"].to_s
-    ($publisher_line1 + "test").should == JSON.parse(response.body)["Address"]["Line1"]
-    ($publisher_line2 + "test").should == JSON.parse(response.body)["Address"]["Line2"]
-    ($publisher_city + "test").should == JSON.parse(response.body)["Address"]["City"]
-    ($publisher_state + "test").should == JSON.parse(response.body)["Address"]["StateProvince"]
-    ($publisher_zip + "test").should == JSON.parse(response.body)["Address"]["PostalCode"]
-    ($publisher_country + "test").should == JSON.parse(response.body)["Address"]["Country"]
+    publisher = @publishers.update(updated_publisher)
+    publisher[:first_name].should eq("Rafael" + @rand)
   end
 
   it "should list all publishers" do
-    result = @@publisher.list()
-    result.length.should > 0
-    result["Items"].last["Id"].to_s.should == $publisher_id
-    result["Items"].last["FirstName"].should == $publisher_first + "test"
-    result["Items"].last["LastName"].should == $publisher_last + "test"
-    result["Items"].last["CompanyName"].should == $publisher_company + "test"
-    result["Items"].last["PaymentOption"].should == "PayPal"
-    result["Items"].last["PaypalEmail"].should == $publisher_paypal_email + "test"
-    
-    result["Items"].last["Address"]["Line1"].should == $publisher_line1 + "test"
-    result["Items"].last["Address"]["Line2"].should == $publisher_line2 + "test"
-    result["Items"].last["Address"]["City"].should == $publisher_city + "test"
-    result["Items"].last["Address"]["StateProvince"].should == $publisher_state + "test"
-    result["Items"].last["Address"]["PostalCode"].should == $publisher_zip + "test"
-    result["Items"].last["Address"]["Country"].should == $publisher_country + "test"
+    publishers = @publishers.list
+
+    publisher = publishers[:items].last
+    publisher[:id].should eq($publisher_id)
   end
 
   it "should delete a new publisher" do
-    response = @@publisher.delete($publisher_id)
+    response = @publishers.delete($publisher_id)
     response.body.should == 'OK'
-  end
-
-  it "should not list deleted publishers" do
-    result = @@publisher.list()
-    result["Items"].each do |r|
-      r["Id"].to_s.should_not == $publisher_id.to_s
-    end
-  end
-
-  it "should not get individual deleted publishers" do
-    response = @@publisher.get($publisher_id)
-    response.body.should == '{"Id":0,"IsDeleted":false}'
-  end
-
-  it "should not update deleted publishers" do
-    $updated_publisher = {
-      'Id' => $publisher_id,
-      'FirstName' => $publisher_first + "test",
-      'LastName' => $publisher_last + "test",
-      'CompanyName' => $publisher_company + "test",
-      'Address' => {
-        'Line1' => $publisher_line1 + "test",
-        'Line2' => $publisher_line2 + "test",
-        'City' => $publisher_city + "test",
-        'StateProvince' => $publisher_state + "test",
-        'PostalCode' => $publisher_zip + "test",
-        'Country' => $publisher_country + "test"
-      },
-      'PaymentOption' => $publisher_payment_option,
-      'PaypalEmail' => $publisher_paypal_email + "test"
-    }
-    response = @@publisher.update($updated_publisher)
-    response.body.should == '{"Id":0,"IsDeleted":false}'
   end
 
   it "should retrieve publisher earnings" do
@@ -171,7 +105,7 @@ describe "Publisher API" do
 
   it "should retrieve publisher payments for a network" do
     payments = { }
-    response = @@publisher.payments payments
+    response = @publishers.payments payments
     #response.first.length.should == 5
   end
 
@@ -180,7 +114,7 @@ describe "Publisher API" do
       'StartDate' => '01/01/2000',
       'EndDate' => '03/01/2000'
     }
-    response = @@publisher.payments payments
+    response = @publishers.payments payments
     response.first.should == nil
   end
 
@@ -188,7 +122,7 @@ describe "Publisher API" do
     payments = { 
       'StartDate' => '01/01/2012'
     }
-    response = @@publisher.payments payments
+    response = @publishers.payments payments
     #response.first.length.should == 5
   end
 
@@ -196,14 +130,14 @@ describe "Publisher API" do
     payments = { 
       #'SiteId' => 6872
     }
-    response = @@publisher.payments payments
+    response = @publishers.payments payments
   end
 
   it "should retrieve publisher payments for a publisher" do
     payments = { 
       #'PublisherAccountId' => 644
     }
-    response = @@publisher.payments payments
+    response = @publishers.payments payments
   end
 
 end
