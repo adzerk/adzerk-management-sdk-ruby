9a5cb02131a9b8a5356db72bb5e5dcd8e3457815
schaney <sean@adzerk.com>
2013-07-30 12:36:15 -0400

    API-42 updating creative/creative map tests to check all returned values

--- test/creative_api_spec.rb
+++ test/creative_api_spec.rb
@@ -39,71 +39,100 @@ describe "Creative API" do
   # end
 
   it "should create a creative using new api fields" do
+
+    $Title = 'Test creative ' + rand(1000000).to_s
+    $Url = "http://adzerk.com"
+    $Body = "Test Body"
+    $AdTypeId = 18
+    $IsHTMLJS = true
+    $ScriptBody = '<html></html>'
+    $IsCreativeActive = true
+    $IsCreativeDeleted = false
+    $Alt = "test alt"
+    $IsSync = false
+
     new_creative = {
-      :title => "Creative with the new API",
+      :title => $Title,
       :image_name => "",
-      :url => "http://adzerk.com",
-      :body => "Test Body",
+      :url => $Url,
+      :body => $Body,
       :advertiser_id => @advertiser_id,
-      :ad_type_id => 18,
-      :is_active => true,
-      :alt => "alt text",
-      :is_deleted => false, 
-      :is_sync => true,
-      :'isHTMLJS' => true,
-      :script_body => "<html></html>"
+      :ad_type_id => $AdTypeId,
+      :is_active => $IsCreativeActive,
+      :alt => $Alt,
+      :is_deleted => $IsCreativeDeleted, 
+      :is_sync => $IsSync,
+      :'isHTMLJS' => $isHTMLJS,
+      :script_body => $ScriptBody
     }
     creative = @creatives.create(new_creative)
     $creative_id = creative[:id].to_s
-    creative[:title].should eq('Creative with the new API')
-    creative[:url].should eq("http://adzerk.com")
-    creative[:body].should eq("Test Body")
+
+    creative[:title].should eq($Title)
+    creative[:url].should eq($Url)
+    creative[:body].should eq($Body)
     creative[:advertiser_id].should eq(@advertiser_id)
-    creative[:ad_type_id].should eq(18)
-    creative[:is_active].should eq(true)
-    creative[:alt].should eq('alt text')
-    creative[:is_deleted].should eq(false)
-    creative[:is_sync].should eq(false)
+    creative[:ad_type_id].should eq($AdTypeId)
+    creative[:is_active].should eq($IsCreativeActive)
+    creative[:alt].should eq($Alt)
+    creative[:is_deleted].should eq($IsCreativeDeleted)
+    creative[:is_sync].should eq($IsSync)
   end
 
   it "should get a specific creative" do
     creative = @creatives.get($creative_id)
-    creative[:title].should eq('Creative with the new API')
-    creative[:url].should eq("http://adzerk.com")
-    creative[:body].should eq("Test Body")
+    creative[:title].should eq($Title)
+    creative[:url].should eq($Url)
+    creative[:body].should eq($Body)
     creative[:advertiser_id].should eq(@advertiser_id)
-    creative[:ad_type_id].should eq(18)
-    creative[:is_active].should eq(true)
-    creative[:alt].should eq('alt text')
-    creative[:is_deleted].should eq(false)
-    creative[:is_sync].should eq(false)
+    creative[:ad_type_id].should eq($AdTypeId)
+    creative[:is_active].should eq($IsCreativeActive)
+    creative[:alt].should eq($Alt)
+    creative[:is_deleted].should eq($IsCreativeDeleted)
+    creative[:is_sync].should eq($IsSync)
   end
 
   it "should update a specific creative" do
+
+    $new_title = "Updated Title"
+    $new_url = "http://adzerk2.com"
+    $new_body = "new body"
+    $new_ad_type_id = 5
+    $new_is_active = false
+    $new_alt = "new alt text"
+    $new_is_sync = false
+
     update_creative = {
       :id => $creative_id.to_i,
-      :title => "Updated Title",
-      :url => 'http://www.google.com',
-      :body => 'new body',
+      :title => $new_title,
+      :url => $new_url,
+      :body => $new_body,
       :advertiser_id => @advertiser_id,
-      :ad_type_id => 18,
-      :is_active => true,
-      :alt => 'new alt text',
+      :ad_type_id => $new_ad_type_id,
+      :is_active => $new_is_active,
+      :alt => $new_alt,
       :is_deleted => false,
-      :is_sync => false
+      :is_sync => $new_is_sync
     }
     creative = @creatives.update(update_creative)
-    creative[:title].should eq('Updated Title')
-    creative[:url].should eq('http://www.google.com')
-    creative[:body].should eq('new body')
+    creative[:title].should eq($new_title)
+    creative[:url].should eq($new_url)
+    creative[:body].should eq($new_body)
+    creative[:ad_type_id].should eq($new_ad_type_id)
+    creative[:is_active].should eq($new_is_active)
+    creative[:is_sync].should eq($new_is_sync)
   end
 
 
   it "should list all creatives for an advertiser" do
     response = @creatives.list(@advertiser_id)
     creative = response[:items].last
-    creative[:url].should eq('http://www.google.com')
-    creative[:body].should eq('new body')
+    creative[:title].should eq($new_title)
+    creative[:url].should eq($new_url)
+    creative[:body].should eq($new_body)
+    creative[:ad_type_id].should eq($new_ad_type_id)
+    creative[:is_active].should eq($new_is_active)
+    creative[:is_sync].should eq($new_is_sync)
   end
 
   it "should delete the creatives after creating it" do
--- test/creative_map_api_spec.rb
+++ test/creative_map_api_spec.rb
@@ -1,4 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'awesome_print'
 
 describe "Creative Flight API" do
 
@@ -15,7 +16,7 @@ describe "Creative Flight API" do
     @zones = client.zones
 
     advertiser = @advertisers.create(:title => "test")
-    @advertiser_id = advertiser[:id].to_s
+    @advertiser_id = advertiser[:id]
 
     channel = @channels.create(:title => 'Test Channel ' + rand(1000000).to_s,
                                :commission => '0.0',
@@ -42,7 +43,7 @@ describe "Creative Flight API" do
              :is_deleted => false)
     @campaign_id = campaign[:id]
 
-     new_flight = {
+    new_flight = {
       :no_end_date => false,
       :priority_id => @priority_id,
       :name => 'Test flight ' + rand(1000000).to_s,
@@ -74,116 +75,203 @@ describe "Creative Flight API" do
   end
 
   it "should create a creative map" do
-    $Title = 'Test creative ' + rand(1000000).to_s
+
     $ImageName = "test.jpg"
-    $Url = "http://adzerk.com"
-    $Body = "Test text"
-    $AdvertiserId = $advertiserId
-    $CampaignId = $campaignId
-    $FlightId = $flightId
-    $MapId = 0
-    $AdTypeId = 18
-    $ZoneId = $zoneId
-    $SiteId = $siteId
+    
     $SizeOverride = false
     $Iframe = false
-    $PublisherAccountId = 372
-    $ScriptBody = '<html>'
-    $Impressions = 100000
+    $Impressions = 10000
     $Percentage = 50
     $DistributionType = 1
+    $IsMapActive = true
+    $IsMapDeleted = false
+    
+    $Title = 'Test creative ' + rand(1000000).to_s
+    $Url = "http://adzerk.com"
+    $Body = "Test text"
+    $AdTypeId = 18
     $IsHTMLJS = true
-    $IsActive = true
+    $ScriptBody = '<html></html>'
+    $IsCreativeActive = true
+    $IsCreativeDeleted = false
     $Alt = "test alt"
-    $IsDeleted = false
     $IsSync = false
 
+    $PublisherAccountId = 372
+    
     new_creative = {
       :campaign_id => @campaign_id,
       :flight_id => @flight_id,
-      :size_override => false,
-      :iframe => false,
-      :impressions => 100000,
-      :percentage => 50,
+      :size_override => $SizeOverride,
+      :iframe => $Iframe,
+      :impressions => $Impressions,
+      :percentage => $Percentage,
       :siteId => @site_id,
       :zoneId => @zone_id,
-      :distributionType => 1,
-      :isActive => true,
-      :isDeleted => false,
+      :distributionType => $DistributionType,
+      :isActive => $IsMapActive,
+      :isDeleted => $IsMapDeleted,
       :creative => {
-        :title => "Creative Title",
-        :url => "http://www.adzerk.com",
-        :body => "Test Body",
+        :title => $Title,
+        :url => $Url,
+        :body => $Body,
         :advertiser_id => @advertiser_id,
-        :ad_type_id => 18,
-        'IsHTMLJS' => true,
-        :script_body => "<html></html>",
-        :is_active => true,
-        :alt => "alt text",
-        :is_deleted => false,
-        :is_sync => false
+        :ad_type_id => $AdTypeId,
+        'IsHTMLJS' => $IsHTMLJS,
+        :script_body => $ScriptBody,
+        :is_active => $IsCreativeActive,
+        :is_deleted => $IsCreativeDeleted,
+        :alt => $Alt,
+        :is_sync => $IsSync
       }
     }
     creative_map = @creative_maps.create(new_creative)
-    creative_map[:creative][:title].should eq("Creative Title")
-    creative_map[:creative][:url].should eq("http://www.adzerk.com")
+
+    # size_override and iframe are left out because they are optional
+    # and always return null
     creative_map[:campaign_id].should eq(@campaign_id)
     creative_map[:flight_id].should eq(@flight_id)
-    creative_map[:zone_id].should eq(@zone_id)
+    #unless distribution type is Fixed (3) this value is ignored
+    creative_map[:impressions].should eq($Impressions)
+    creative_map[:percentage].should eq($Percentage)
     creative_map[:site_id].should eq(@site_id)
-    creative_map[:creative][:is_htmljs]
-    creative_map[:creative][:script_body].should eq("<html></html>")
-    $creative_id_id = creative_map[:creative][:id]
+    creative_map[:zone_id].should eq(@zone_id)
+    creative_map[:distribution_type].should eq($DistributionType)
+    creative_map[:is_active].should eq($IsMapActive)
+    creative_map[:is_deleted].should eq($IsMapDeleted)
+
+    creative_map[:creative][:title].should eq($Title)
+    creative_map[:creative][:url].should eq($Url)
+    creative_map[:creative][:body].should eq($Body)
+    creative_map[:creative][:advertiser_id].should eq(@advertiser_id)
+    creative_map[:creative][:ad_type_id].should eq($AdTypeId)
+    creative_map[:creative][:is_htmljs].should eq($IsHTMLJS)
+    creative_map[:creative][:script_body].should eq($ScriptBody)
+    creative_map[:creative][:is_active].should eq($IsCreativeActive)
+    creative_map[:creative][:is_deleted].should eq($IsCreativeDeleted)
+    creative_map[:creative][:alt].should eq($Alt)
+    creative_map[:creative][:is_sync].should eq($IsSync)
+
+    $creative_id = creative_map[:creative][:id]
   end
 
   it "should list all creatives maps for a flight" do
     creative_maps = @creative_maps.list(@flight_id)
     creative_map = creative_maps[:items].last
     $map_id= creative_map[:id]
-    creative_map[:creative][:title].should eq("Creative Title")
-    creative_map[:creative][:url].should eq("http://www.adzerk.com")
+
     creative_map[:campaign_id].should eq(@campaign_id)
     creative_map[:flight_id].should eq(@flight_id)
-    creative_map[:zone_id].should eq(@zone_id)
+    creative_map[:impressions].should eq($Impressions)
+    creative_map[:percentage].should eq($Percentage)
     creative_map[:site_id].should eq(@site_id)
-    creative_map[:creative][:is_htmljs]
-    creative_map[:creative][:script_body].should eq("<html></html>")
+    creative_map[:zone_id].should eq(@zone_id)
+    creative_map[:distribution_type].should eq($DistributionType)
+    creative_map[:is_active].should eq($IsMapActive)
+    creative_map[:is_deleted].should eq($IsMapDeleted)
+
+    creative_map[:creative][:title].should eq($Title)
+    creative_map[:creative][:url].should eq($Url)
+    creative_map[:creative][:body].should eq($Body)
+    creative_map[:creative][:advertiser_id].should eq(@advertiser_id)
+    creative_map[:creative][:ad_type_id].should eq($AdTypeId)
+    creative_map[:creative][:is_htmljs].should eq($IsHTMLJS)
+    creative_map[:creative][:script_body].should eq($ScriptBody)
+    creative_map[:creative][:is_active].should eq($IsCreativeActive)
+    creative_map[:creative][:is_deleted].should eq($IsCreativeDeleted)
+    creative_map[:creative][:alt].should eq($Alt)
+    creative_map[:creative][:is_sync].should eq($IsSync)
   end
 
   it "should get a specific creative map" do
     creative_map = @creative_maps.get($map_id, @flight_id)
-    creative_map[:creative][:title].should eq("Creative Title")
-    creative_map[:creative][:url].should eq("http://www.adzerk.com")
+    
     creative_map[:campaign_id].should eq(@campaign_id)
     creative_map[:flight_id].should eq(@flight_id)
-    creative_map[:zone_id].should eq(@zone_id)
+    creative_map[:impressions].should eq($Impressions)
+    creative_map[:percentage].should eq($Percentage)
     creative_map[:site_id].should eq(@site_id)
-    creative_map[:creative][:is_htmljs]
-    creative_map[:creative][:script_body].should eq("<html></html>")
+    creative_map[:zone_id].should eq(@zone_id)
+    creative_map[:distribution_type].should eq($DistributionType)
+    creative_map[:is_active].should eq($IsMapActive)
+    creative_map[:is_deleted].should eq($IsMapDeleted)
+
+    creative_map[:creative][:title].should eq($Title)
+    creative_map[:creative][:url].should eq($Url)
+    creative_map[:creative][:body].should eq($Body)
+    creative_map[:creative][:advertiser_id].should eq(@advertiser_id)
+    creative_map[:creative][:ad_type_id].should eq($AdTypeId)
+    creative_map[:creative][:is_htmljs].should eq($IsHTMLJS)
+    creative_map[:creative][:script_body].should eq($ScriptBody)
+    creative_map[:creative][:is_active].should eq($IsCreativeActive)
+    creative_map[:creative][:is_deleted].should eq($IsCreativeDeleted)
+    creative_map[:creative][:alt].should eq($Alt)
+    creative_map[:creative][:is_sync].should eq($IsSync)
  end
 
-  it "should update a specific creative" do
-   updated_creative = {
+  it "should update a specific creative map" do
+    
+    new_impressions = 1234
+    new_percentage = 51
+    new_is_active = false
+
+    updated_creative = {
       :id => $map_id,
       :campaign_id => @campaign_id,
       :flight_id => @flight_id,
-      :size_override => false,
-      :iframe => false,
-      :impressions => 200000,
-      :percentage => 50,
+      :size_override => $SizeOverride,
+      :iframe => $iframe,
+      :impressions => new_impressions,
+      :percentage => new_percentage,
       :site_id => @site_id,
       :zone_id => @zone_id,
-      :is_active => true,
+      :distribution_type => $DistributionType,
+      :is_active => new_is_active,
       :is_deleted => false,
       :creative => {
-        :id => $creative_id_id
+        :id => $creative_id
       }
     }
     creative_map = @creative_maps.update(updated_creative)
-    creative_map[:impressions].should eq(200000)
+    
+    #test new values
+    creative_map[:percentage].should eq(new_percentage)
+    creative_map[:impressions].should eq(new_impressions)
+    creative_map[:is_active].should eq(new_is_active)
+
+    #make sure old values are unchanged
+    creative_map[:campaign_id].should eq(@campaign_id)
+    creative_map[:flight_id].should eq(@flight_id)
+    creative_map[:site_id].should eq(@site_id)
+    creative_map[:zone_id].should eq(@zone_id)
+    creative_map[:distribution_type].should eq($DistributionType)
+    creative_map[:is_deleted].should eq($IsMapDeleted)
+
+    creative_map[:creative][:title].should eq($Title)
+    creative_map[:creative][:url].should eq($Url)
+    creative_map[:creative][:body].should eq($Body)
+    creative_map[:creative][:advertiser_id].should eq(@advertiser_id)
+    creative_map[:creative][:ad_type_id].should eq($AdTypeId)
+    creative_map[:creative][:is_htmljs].should eq($IsHTMLJS)
+    creative_map[:creative][:script_body].should eq($ScriptBody)
+    creative_map[:creative][:is_active].should eq($IsCreativeActive)
+    creative_map[:creative][:is_deleted].should eq($IsCreativeDeleted)
+    creative_map[:creative][:alt].should eq($Alt)
+    creative_map[:creative][:is_sync].should eq($IsSync)
   end
 
-  it "should update the scriptBody tag on a nested creative" do
+  it "should update the nested creative" do
+
+    new_title = "new title"
+    new_url = "http://newurl.com"
+    new_body = "new_body"
+    new_ad_type_id = 5
+    new_script_body = "<html>new body</html>"
+    new_is_active = false
+    new_alt = "new alt text"
+    new_is_sync = true
+    new_image_name = "new image name"
+
     updated_creative = {
       :id => $map_id,
       :campaign_id => @campaign_id,
@@ -197,12 +285,32 @@ describe "Creative Flight API" do
       :is_active => true,
       :is_deleted => false,
       :creative => {
-        :id => $creative_id_id,
-        :script_body => '<html>New Body</html>',
+        :id => $creative_id,
+        :title => new_title,
+        :url => new_url,
+        :body => new_body,
+        :advertiser_id => @advertiser_id,
+        :ad_type_id => new_ad_type_id,
+        'IsHTMLJS' => true,
+        :script_body => new_script_body,
+        :is_active => new_is_active,
+        :alt => new_alt,
+        :is_deleted => false,
+        :is_sync => new_is_sync,
+        :image_name => new_image_name
       }
     }
     creative_map = @creative_maps.update(updated_creative)
-    creative_map[:creative][:script_body].should eq('<html>New Body</html>')
+    creative_map[:creative][:title].should eq new_title
+    creative_map[:creative][:url].should eq new_url
+    creative_map[:creative][:body].should eq new_body
+    creative_map[:creative][:ad_type_id].should eq new_ad_type_id
+    creative_map[:creative][:script_body].should eq new_script_body
+    creative_map[:creative][:is_active].should eq new_is_active
+    creative_map[:creative][:alt].should eq new_alt
+    creative_map[:creative][:is_sync].should eq new_is_sync
+    creative_map[:creative][:image_name].should eq new_image_name
+    
   end
 
   it "should delete the creatives after creating it" do
@@ -233,7 +341,7 @@ describe "Creative Flight API" do
         :is_active => true,
         :is_deleted => false,
         :creative => {
-          :id => $creative_id_id
+          :id => $creative_id
         }
       )
     }.should raise_error "This flight is not part of your network"
@@ -254,7 +362,7 @@ describe "Creative Flight API" do
         :is_active => true,
         :is_deleted => false,
         :creative => {
-          :id => $creative_id_id
+          :id => $creative_id
         }
       )
     }.should raise_error "This creative map has been deleted"
