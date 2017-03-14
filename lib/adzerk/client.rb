66e756c9c8e6c88d667b1d71ba8784a29751c10e
Dave Yarwood <dave.yarwood@gmail.com>
2017-03-14 11:03:16 -0400

    fix warning about Content-Type header actually being application/x-www-form-urlencoded
    The "Create Creative" API endpoint expects application/x-www-form-urlencoded,
    not application/json. Fortunately, the Ruby gem has been correctly using
    x-www-form-urlencoded despite the params including `:content_type => :json`,
    because the rest-client library uses x-www-form-urlencoded by default unless you
    call `.to_json` on the body of the entire request.
    
    (Note that there is a `.to_json` in the code here, but what we're actually
    sending is what the API expects, which is x-www-form-urlencoded data where the
    field "creative" is that JSON string.)
    
    It generates a warning though because we're telling it to send JSON and it's
    sending x-www-form-urlencoded data. This commit fixes the warning by not telling
    it that we're sending JSON, because we're not.

--- lib/adzerk/client.rb
+++ lib/adzerk/client.rb
@@ -63,9 +63,8 @@ module Adzerk
     def create_creative(data={}, image_path='')
       response = RestClient.post(@config[:host] + 'creative',
                                  {:creative => camelize_data(data).to_json},
-                                 :X_Adzerk_ApiKey => @api_key,
-                                 :content_type => :json,
-                                 :accept => :json)
+                                  :X_Adzerk_ApiKey => @api_key,
+                                  :accept => :json)
       response = upload_creative(JSON.parse(response)["Id"], image_path) unless image_path.empty?
       response
     end
