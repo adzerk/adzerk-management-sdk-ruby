3bc79b8d36f2c10039f1a283e87eedcd40e4ef94
Justin Niessner <CrshOverride@gmail.com>
2020-12-21 12:58:17 -0500

    Keep form post body for publisher endpoint

--- lib/adzerk/publisher.rb
+++ lib/adzerk/publisher.rb
@@ -13,5 +13,19 @@ module Adzerk
       parse_response(response)
     end
 
+    def create(opts={}, subid=nil)
+      e = (subid && subendpoint) ? "#{subendpoint}/#{subid}/#{endpoint}" : endpoint
+      data = { datakey => camelize_data(opts).to_json }
+      response = @client.post_request(e, data)
+      parse_response(response)
+    end
+
+    def update(opts={})
+      id = opts[:id].to_s
+      data = { datakey => camelize_data(opts).to_json }
+      response = @client.put_request("#{endpoint}/#{id}", data)
+      parse_response(response)
+    end
+
   end
 end
