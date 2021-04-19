36fbfe6231435ac6b497b150712655ef909b66c8
Callie Bensel <calliebensel@Callies-MacBook-Pro.local>
2021-04-19 15:37:07 -0500

    CH19708/Add search campaign method

--- lib/adzerk/campaign.rb
+++ lib/adzerk/campaign.rb
@@ -4,5 +4,11 @@ module Adzerk
       url = "instantcounts/#{endpoint}/#{campaign_id}"
       parse_response(client.get_request(url))
     end
+
+    def search(campaign_name)
+      url = 'campaign/search'
+      data = { 'campaignName' => campaign_name }
+      parse_response(client.post_request(url, data))
+    end
   end
 end
