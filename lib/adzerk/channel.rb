ff03552724e6ea109a18a454e536f1bbc8d1364d
Callie Bensel <calliebensel@Callies-MacBook-Pro.local>
2021-04-23 11:32:56 -0500

    CH19710/Add get priorities for channel

--- lib/adzerk/channel.rb
+++ lib/adzerk/channel.rb
@@ -2,7 +2,8 @@ module Adzerk
     class Channel < ApiEndpoint
         def get_priorities(channel_id)
             url = "channel/#{channel_id}/priorities"
-            client.get_request(url)
+            response = client.get_request(url)
+            JSON.parse(response.body)
         end
     end
 end
